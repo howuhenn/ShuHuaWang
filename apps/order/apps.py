222c36278ad20a868cb7dbf12bc2559c859dad1e
fertast <fertast@sina.com>
2019-06-30 23:15:12 +0800

    up

---
+++ apps/order/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Order1Config(AppConfig):
+    name = 'apps.order'
---
+++ apps/user/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+# Create your views here.
+
+
+class TestView(View):
+
+    def get(self, request):
+
+        return render(request, 'login.html')
